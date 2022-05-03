c48d043ec287f0c1fe87388d04d2f50ca207a33e
al-wazny <ali.al-wazny@flagbit.de>
2022-05-03 12:27:54 +0200

    chore: adjust for windows

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -8,23 +8,12 @@ class Pages extends Controller {
 
     public function __construct()
     {
-        parent::__construct();
-        $this->pagesModel = $this->model('Pages_data');
-        $this->todoModel = $this->model('TodoModel');
         $this->authModel = $this->model('authenticationModel');
     }
 
     public function index () // default method if there is no method
     {
-        $posts = $this->pagesModel->getTodos();
 
-        $this->todoModel->emptyTypes();
-
-        $posts = $this->authModel->isAuthor($posts);
-
-        $data = [
-            'task' => $posts,
-        ];
 
         $this->view('index', $data);
     }
