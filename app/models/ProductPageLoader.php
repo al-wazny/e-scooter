e4c46e5fbb06fe6ec92c7ec162644dfb529f1316
Anwar Chaban <anwarshabn95@outlook.com>
2022-05-12 21:25:35 +0200

    update

---
+++ app/models/ProductPageLoader.php
@@ -0,0 +1,16 @@
+<?php
+
+class authenticationModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function product()
+    {
+        
+    }
+}
