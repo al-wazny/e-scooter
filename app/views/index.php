d2f83159ab815465cb97c0e2e2bae2f0f0c7c3f4
anwar.chaban <anwar.chaban@ktc.de>
2022-05-17 12:33:22 +0200

    add titel and images

--- app/views/index.php
+++ app/views/index.php
@@ -4,7 +4,8 @@
     <head>
         <link rel="stylesheet" href="assets/css/styles.css" />
         <link rel="stylesheet" href="assets/css/scooters.css" />
-    </head>
+        <title>Automic Scooter</title>
+     </head>
     <body>
         <header>
             <?php include('app/views/pagelets/header.php') ?>
