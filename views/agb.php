e166e942ab21e7c35f520c47682cd1c4a85ebf05
chickensoup2077 <103024215+chickensoup2077@users.noreply.github.com>
2022-06-20 11:15:32 +0200

    css impressum...

---
+++ views/agb.php
@@ -0,0 +1,154 @@
+<?php include('../app/controllers/Pages.php'); ?>
+
+<html>
+    <head>
+        <!---//TODO-->
+        <link rel="stylesheet" href="../assets/css/index.css">
+        <link rel="stylesheet" href="../assets/css/burger.css">
+        <link rel="stylesheet" href="../assets/css/agb.css">
+        <title>ABG</title>
+    </head>
+    <body>
+        <div id="container">   
+            <header>
+            
+
+            <a class="CompanyLogo" href="../views/index.php">  
+                   <img class="CompanyLogo" src="../assets/Images/AtomLogo.png" alt="Company Logo">
+                </a>
+               <a class="register" href=" " target="_top" >Registrieren</a>
+                <a class="login" href=" " target="_top">Einloggen</a> 
+                <div class="bar">
+                    <div class="Categoriesbar">
+                        <button>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </button>
+                    </div>
+                </div>
+             
+            </header>
+
+
+            <div class="agb-text">
+                
+<p class="text-titel">Allgemeine Verkaufsbedingungen</p>
+<p class="unter-titel">Geltungsbereich</p>
+
+
+
+Diese Geschäftsbedingungen gelten für alle Käufe bei <a href="../views/impressum.php">Automic Scooters</a>, die von Privatkunden getätigt werden. 
+Privatkunden in diesem Sinne sind Personen mit Wohnsitz und Lieferadresse in der Bundesrepublik Deutschland, soweit die von ihnen bestellten Waren weder ihrer gewerblichen noch ihrer selbstständigen beruflichen Tätigkeit zugerechnet werden können. 
+Geschäftskunden werden gebeten, Bestellungen auf den über den Geschäftskunden-Login erreichbaren Bestellseiten <!-----------[Link auf Geschäftskunden-Login]------> zu tätigen.
+
+
+
+<p class="unter-titel">Preise und Versandkosten</p>
+Die ausgezeichneten Preise sind Endpreise inkl. Umsatzsteuer. Es gilt der Betrag, der jeweils zum Zeitpunkt der verbindlichen Bestellung ausgewiesen ist. Hinzu kommen Versandkosten, die von der Versandart und der Größe und dem Gewicht der von Ihnen bestellten Ware(n) abhängig sind. Über die Einzelheiten können Sie sich unter [Link auf „Versandkosten"] informieren. Die regelmäßigen Kosten der Rücksendung, die im Falle einer Rückgabe der Ware durch Sie in Ausübung Ihres Widerrufsrechts [Link auf „Widerrufsrecht"] entstehen, tragen wir. Bei Ausübung Ihres Widerrufsrechts erstatten wir Ihnen auch die Versandkosten zurück.
+
+
+
+<p class="unter-titel">Zahlung</p>
+Die Bezahlung erfolgt bei Lieferung mittels 
+	- PayPal 
+
+
+
+    <p class="unter-titel">Zahlungsverzug</p>
+Kommen Sie in Zahlungsverzug, so ist Automic Scooters berechtigt, Verzugszinsen in Höhe von 5 Prozentpunkten über dem von der Deutschen Bundesbank für den Zeitpunkt der Bestellung bekannt gegebenen Basiszinssatz p.a. zu fordern. Falls Automic Scooters ein höherer Verzugsschaden nachweisbar entstanden ist, ist Automic Scooters berechtigt, diesen geltend zu machen.
+
+
+
+<p class="unter-titel">Zurückbehaltungsrecht</p>
+Zur Ausübung eines Zurückbehaltungsrechts ist der Kunde nur insoweit befugt, als sein Gegenanspruch auf demselben Vertragsverhältnis beruht.
+
+
+
+<p class="unter-titel">Lieferung</p>
+(1) Die Lieferung erfolgt an die vom Kunden angegebene Lieferanschrift, innerhalb von 
+	- Deutschland 
+(2) Macht höhere Gewalt (Naturkatastrophen, Krieg, Bürgerkrieg, Terroranschlag) die Lieferung oder eine sonstige Leistung dauerhaft unmöglich, ist eine Leistungspflicht von Automic Scooters ausgeschlossen. Bereits gezahlte Beträge werden von Automic Scooters unverzüglich erstattet.
+(3) Automic Scooters kann außerdem die Leistung verweigern, soweit diese einen Aufwand erfordert, der unter Beachtung des Inhalts des Kaufvertrages und der Gebote von Treu und Glauben in einem groben Missverhältnis zu dem Interesse des Kunden an der Erfüllung des Kaufvertrages steht. Bereits gezahlte Beträge werden von Automic Scooters unverzüglich erstattet.
+(4) Sperrgut (Pakete mit einem größeren Volumen als 1 qm) werden in der Regel per Spedition geliefert. Automic Scooters weist ausdrücklich darauf hin, dass diese Ware nicht ins Haus getragen wird. 
+
+
+
+<p class="unter-titel">Günstige Versandart bei Rücksendung</p>
+(1) Bitte verwenden Sie bei der Rücksendung der Ware und des Zubehörs nach Möglichkeit die Originalverpackung, auch wenn diese durch eine Öffnung zur Funktionsprüfung beschädigt sein sollte.
+(2) Bitte verwenden Sie zur Rücksendung den der Warenlieferung beigefügten, vollständig frankierten und adressierten Rücksendeaufkleber. Es handelt sich dabei um die einfachste und kostengünstigste Versandmöglichkeit. Sie trifft keine Verpflichtung zur Verwendung dieses Rücksendeverfahrens. Wenn Sie allerdings eine unnötig teure Versandart wählen, können Sie unter Umständen verpflichtet sein, die gegenüber einer günstigen Versandart erhöhten Kosten an uns zu zahlen. 
+
+
+
+<p class="unter-titel">Eigentumsvorbehalt</p>
+Bis zur vollständigen Begleichung aller gegen den Kunden bestehender Ansprüche aus dem Kaufvertrag bleibt die gelieferte Ware im Eigentum von Automic Scooters. Solange dieser Eigentumsvorbehalt besteht, darf der Kunde die Ware weder weiterveräußern noch über die Ware verfügen; insbesondere darf der Kunde Dritten vertraglich keine Nutzung an der Ware einräumen. 
+
+
+
+<p class="unter-titel">Mängelrechte</p>
+(1) Ein bereits bei der Lieferung mangelhaftes Produkt (Gewährleistungsfall) wird Automic Scooters nach Wahl des Kunden auf Kosten von Automic Scooters durch ein mangelfreies ersetzen oder fachgerecht reparieren lassen (Nacherfüllung). Der Kunde wird darauf hingewiesen, dass kein Gewährleistungsfall vorliegt, wenn das Produkt bei Gefahrübergang die vereinbarte Beschaffenheit hatte. Ein Gewährleistungsfall liegt insbesondere in folgenden Fällen nicht vor:
+a)	bei Schäden, die beim Kunden durch Missbrauch oder unsachgemäßen Gebrauch entstanden sind,
+b)	bei Schäden, die dadurch entstanden sind, dass die Produkte beim Kunden schädlichen äußeren Einflüssen ausgesetzt worden sind (insbesondere extremen Temperaturen, Feuchtigkeit, außergewöhnlicher physikalischer oder elektrischer Beanspruchung, Spannungsschwankungen, Blitzschlag, statischer Elektrizität, Feuer).
+(2) Automic Scooters leistet ferner keine Gewähr für einen Fehler, der durch unsachgemäße Reparatur durch einen nicht vom Hersteller autorisierten Servicepartner entstanden ist.
+(3) Erfordert die vom Kunden gewünschte Art der Nacherfüllung (Ersatzlieferung oder Reparatur) einen Aufwand, der in Anbetracht des Produktpreises unter Beachtung des Vertragsinhaltes und der Gebote von Treu und Glauben in einem groben Missverhältnis zu dem Leistungsinteresse des Kunden steht – wobei insbesondere der Wert des Kaufgegenstandes im mangelfreien Zustand, die Bedeutung des Mangels und die Frage zu berücksichtigen sind, ob auf die andere Art der Nacherfüllung ohne erhebliche Nachteile für den Kunden zurückgegriffen werden kann – beschränkt sich der Anspruch des Kunden auf die jeweils andere Art der Nacherfüllung. Das Recht von Automic Scooters, auch diese andere Art der Nacherfüllung unter der vorgenannten Voraussetzung zu verweigern, bleibt unberührt. 
+(4) Sowohl für den Fall der Reparatur als auch für den Fall der Ersatzlieferung ist der Kunde verpflichtet, das Produkt auf Kosten von Automic Scooters unter Angabe der Auftragsnummer an die von ihr angegebene Rücksendeadresse einzusenden. Vor der Einsendung hat der Kunde von ihm eingefügte Gegenstände aus dem Produkt zu entfernen. Automic Scooters ist nicht verpflichtet, das Produkt auf den Einbau solcher Gegenstände zu untersuchen. Für den Verlust solcher Gegenstände haftet Automic Scooters nicht, es sei denn, es war bei Rücknahme des Produktes für Automic Scooters ohne Weiteres erkennbar, dass ein solcher Gegenstand in das Produkt eingefügt worden ist (in diesem Fall informiert Automic Scooters den Kunden und hält den Gegenstand für den Kunden zur Abholung bereit; der Kunde trägt die dabei entstehenden Kosten). Der Kunde hat zudem, bevor er ein Produkt zur Reparatur oder zum Austausch einsendet, gegebenenfalls separate Sicherungskopien der auf dem Produkt befindlichen Systemsoftware, der Anwendungen und aller Daten auf einem separaten Datenträger zu erstellen und alle Passwörter zu deaktivieren. Eine Haftung für Datenverlust wird nicht übernommen. Ebenso obliegt es dem Kunden, nachdem ihm das reparierte Produkt oder das Ersatzprodukt zurückgesandt worden ist, die Software und Daten zu installieren und die Passwörter zu reaktivieren. 
+(5) Sendet der Kunde die Ware ein, um ein Austauschprodukt zu bekommen, richtet sich die Rückgewähr des mangelhaften Produktes nach folgender Maßgabe: Sofern der Kunde die Ware zwischen Lieferung und Rücksendung in mangelfreiem Zustand benutzen konnte, hat dieser den Wert der von ihm gezogenen Nutzungen zu erstatten. Für einen nicht durch den Mangel eingetretenen Untergang oder die weitere Verschlechterung der Ware sowie für die nicht durch den Mangel eingetretene Unmöglichkeit der Herausgabe der Ware im Zeitraum zwischen Lieferung der Ware und Rücksendung der Ware hat der Kunde Wertersatz zu leisten. Der Kunde hat keinen Wertersatz für die durch den bestimmungsgemäßen Gebrauch der Ware entstandene Verschlechterung der Ware zu leisten. Die Pflicht zum Wertersatz entfällt für die Rücksendung eines mangelhaften Produktes im Gewährleistungsfall ferner,
+a)	wenn sich der zum Rücktritt berechtigende Mangel erst während der Verarbeitung oder Umgestaltung gezeigt hat,
+b)	wenn Automic Scooters die Verschlechterung oder den Untergang zu vertreten hat oder der Schaden auch bei Automic Scooters eingetreten wäre,
+c)	wenn die Verschlechterung oder der Untergang beim Kunden eingetreten ist, obwohl dieser diejenige Sorgfalt beachtet hat, die er in eigenen Angelegenheiten anzuwenden pflegt.
+(6) Die Schadensersatzpflicht des Kunden bei einer vom Kunden zu vertretenden Verletzung der Rücksendungspflicht richtet sich nach Maßgabe der gesetzlichen Bestimmungen.
+(7) Der Kunde kann nach seiner Wahl vom Vertrag zurücktreten oder den Kaufpreis mindern, wenn die Reparatur oder Ersatzlieferung innerhalb einer angemessenen Frist nicht zu einem vertragsgerechten Zustand des Produktes geführt hat. 
+(8) Darüber hinaus können auch Ansprüche gegen den Hersteller im Rahmen einer von diesem eingeräumten Garantie bestehen, die sich nach den entsprechenden Garantiebedingungen richten. 
+(9) Die gesetzliche Gewährleistung von Automic Scooters endet zwei Jahre ab Lieferung der Ware. Die Frist beginnt mit dem Erhalt der Ware. 
+
+<p class="unter-titel">Haftung</p>
+(1) Bei leichter Fahrlässigkeit haftet Automic Scooters nur bei der Verletzung vertragswesentlicher Pflichten und beschränkt auf den vorhersehbaren Schaden. Diese Beschränkung gilt nicht bei der Verletzung von Leben, Körper und Gesundheit. Für sonstige leicht fahrlässig durch einen Mangel des Kaufgegenstandes verursachte Schäden haftet Automic Scooters nicht.
+(2) Unabhängig von einem Verschulden von Automic Scooters bleibt eine Haftung von Automic Scooters bei arglistigem Verschweigen des Mangels oder aus der Übernahme einer Garantie unberührt. Die Herstellergarantie ist eine Garantie des Herstellers und stellt keine Übernahme einer Garantie durch Automic Scooters dar.
+(3) Automic Scooters ist auch für die während ihres Verzugs durch Zufall eintretende Unmöglichkeit der Lieferung verantwortlich, es sei denn, dass der Schaden auch bei rechtzeitiger Lieferung eingetreten wäre.
+(4) Ausgeschlossen ist die persönliche Haftung der gesetzlichen Vertreter, Erfüllungsgehilfen und Betriebsangehörigen von Automic Scooters für von ihnen durch leichte Fahrlässigkeit verursachte Schäden. 
+
+
+
+<p class="unter-titel">Anwendbares Recht</p>
+Der zwischen Ihnen und Automic Scooters abgeschlossene Vertrag unterliegt ausschließlich dem Recht der Bundesrepublik Deutschland unter ausdrücklichem Ausschluss des UN-Kaufrechts. Unberührt davon bleiben die zwingenden Bestimmungen des Staates, in dem Sie Ihren gewöhnlichen Aufenthalt haben.
+
+
+
+<p class="unter-titel">Gerichtsstand</p>
+Sofern Sie entgegen Ihren Angaben bei der Bestellung keinen Wohnsitz in der Bundesrepublik Deutschland haben oder nach Vertragsabschluss Ihren Wohnsitz ins Ausland verlegen oder Ihr Wohnsitz zum Zeitpunkt der Klageerhebung nicht bekannt ist, ist Gerichtsstand für alle Streitigkeiten aus und im Zusammenhang mit dem Vertragsverhältnis [Sitz des Betreibers des Online-Shops]. 
+
+
+
+<p class="unter-titel">Jugendschutz</p>
+Sollten Waren gekauft werden, die dem Jugendschutz unterliegen, wird durch geeignete Maßnahmen eine Altersverifikation durchgeführt, um die Identität und das Alter des Käufers zu überprüfen. Eine Übergabe der Ware erfolgt nur, wenn der Kunde die Volljährigkeit nachweisen konnte. 
+
+
+
+<p class="unter-titel">Streitbeilegung</p>
+Allgemeine Informationspflichten zur alternativen Streitbeilegung nach Art. 14 Abs. 1 ODR-VO und § 36 VSBG (Verbraucherstreitbeilegungsgesetz):
+Die europäische Kommission stellt eine Plattform zur Online-Streitbelegung (OS) zur Verfügung, die Sie unter dieser Adresse finden: http://ec.europa.eu/consumers/odr/ . Zur Teilnahme an einem Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle sind wir nicht verpflichtet und auch nicht bereit. 
+Schlussbestimmungen
+(1) Sollten einzelne Bestimmungen dieses Vertrages ganz oder teilweise unwirksam oder nichtig sein oder werden, so wird dadurch die Wirksamkeit des Vertrages im Übrigen nicht berührt, insoweit ein Vertragspartner hierdurch nicht unangemessen benachteiligt wird.
+(2) Änderungen oder Ergänzungen dieses Vertrages bedürfen der Schriftform.
+
+
+
+            </div>
+
+            <div class="footer">
+         <a class="agb" href="../views/agb.php" target="_bottom">AGB</a>
+         <a class="impressum" href="../views/datenschutzerklaerung.php" target="_bottom">Datenschutzerklärung</a>
+         <a class="impressum" href="../views/impressum.php" target="_bottom">Impressum</a>
+        
+
+
+
+        </div>
+
+
+
+
+
+
+        </body>
+</html>
--- views/index.php
+++ views/index.php
@@ -11,12 +11,13 @@
     <body>
         <div id="container">   
             <header>
-                <a class="CompanyLogo" href="header\header.html"> 
+            
+
+            <a class="CompanyLogo" href="../../views/index.php"> 
                    <img class="CompanyLogo" src="../assets/Images/AtomLogo.png" alt="Company Logo">
                 </a>
-                <a class="register" href=" " target="_top">Registrieren</a>
-                <a class="login" href=" " target="_top">Einloggen</a>
-
+               <a class="register" href=" " target="_top" >Registrieren</a>
+                <a class="login" href=" " target="_top">Einloggen</a> 
                 <div class="bar">
                     <div class="Categoriesbar">
                         <button>
@@ -26,6 +27,7 @@
                         </button>
                     </div>
                 </div>
+             
             </header>
                
                <!--------------------------------------------------Slider js script----------------------------------------------> 
@@ -68,6 +70,86 @@
             <div class="beispielprodukte">
                 <?php foreach($data as $scooter): ?>
                     <a href="">
+                        
+                    <div class="singlescooter product1">
+                    <div class="scooterimg 1">
+                                <img   src="https://res.cloudinary.com/grover/image/upload/e_trim/f_webp,q_auto/b_white,c_pad,h_300,w_300/v1650546624/c2ujbogyzmfgtlxpovzi.png" alt="Segway Ninebot F40D E-Scooter">    
+                            </div>
+                            <div class="singlescooterhead 1">
+                                <h3 style="color: black;"><?= $scooter['title'] ?></h3><br>
+                            </div>
+                            <div class="singlescooterinfo 1">
+                                Versicherung und Nummernschild inklusive,<br>
+                                40km Reichweite, 17.1kg, Bluetooth app
+                            </div>
+                            <div class="scooterbutton 1">
+                                <button type=" button">Jetzt Mieten</button>
+                            </div>
+                            <div class="singlescooterprice 1">
+                                <b style="color: rgb(0, 0, 0); font-style: italic; font-size: small; "> &nbsp;pro stunde &nbsp;<b style="color: red; font-style: italic; font-weight: 600; ">0,22 € </b></b>
+                            </div>
+                            
+                        </div>
+                    </a>
+                <?php endforeach; ?>
+                
+                
+                
+                
+                <?php foreach($data as $scooter): ?>
+                    <a href="">
+                        <div class="singlescooter product1">
+                            <div class="singlescooterhead 1">
+                                <h3 style="color: black;"><?= $scooter['title'] ?></h3><br>
+                            </div>
+                            <div class="singlescooterinfo 1">
+                                Versicherung und Nummernschild inklusive,<br>
+                                40km Reichweite, 17.1kg, Bluetooth app
+                            </div>
+                            <div class="scooterbutton 1">
+                                <button type=" button">Jetzt Mieten</button>
+                            </div>
+                            <div class="singlescooterprice 1">
+                                <b style="color: rgb(0, 0, 0); font-style: italic; font-size: small; "> &nbsp;pro stunde &nbsp;<b style="color: red; font-style: italic; font-weight: 600; ">0,22 € </b></b>
+                            </div>
+                            <div class="scooterimg 1">
+                                <img   src="https://res.cloudinary.com/grover/image/upload/e_trim/f_webp,q_auto/b_white,c_pad,h_300,w_300/v1650546624/c2ujbogyzmfgtlxpovzi.png" alt="Segway Ninebot F40D E-Scooter">    
+                            </div>
+                        </div>
+                    </a>
+                <?php endforeach; ?>
+                
+                
+                
+                
+                <?php foreach($data as $scooter): ?>
+                    <a href="">
+                        <div class="singlescooter product1">
+                            <div class="singlescooterhead 1">
+                                <h3 style="color: black;"><?= $scooter['title'] ?></h3><br>
+                            </div>
+                            <div class="singlescooterinfo 1">
+                                Versicherung und Nummernschild inklusive,<br>
+                                40km Reichweite, 17.1kg, Bluetooth app
+                            </div>
+                            <div class="scooterbutton 1">
+                                <button type=" button">Jetzt Mieten</button>
+                            </div>
+                            <div class="singlescooterprice 1">
+                                <b style="color: rgb(0, 0, 0); font-style: italic; font-size: small; "> &nbsp;pro stunde &nbsp;<b style="color: red; font-style: italic; font-weight: 600; ">0,22 € </b></b>
+                            </div>
+                            <div class="scooterimg 1">
+                                <img   src="https://res.cloudinary.com/grover/image/upload/e_trim/f_webp,q_auto/b_white,c_pad,h_300,w_300/v1650546624/c2ujbogyzmfgtlxpovzi.png" alt="Segway Ninebot F40D E-Scooter">    
+                            </div>
+                        </div>
+                    </a>
+                <?php endforeach; ?>
+               
+               
+               
+               
+                <?php foreach($data as $scooter): ?>
+                    <a href="">
                         <div class="singlescooter product1">
                             <div class="singlescooterhead 1">
                                 <h3 style="color: black;"><?= $scooter['title'] ?></h3><br>
@@ -89,6 +171,22 @@
                     </a>
                 <?php endforeach; ?>
             </div>
+            
+
+
+
+
+
+
+        </div>
+        <div class="footer">
+         <a class="agb" href="../views/agb.php" target="_bottom">AGB</a>
+         <a class="impressum" href="../views/datenschutzerklaerung.php" target="_bottom">datenschutzerklaerung</a>
+         <a class="impressum" href="../views/impressum.php" target="_bottom">Impressum</a>
+        
+
+
+
         </div>
     </body>
     <script src="../assets/JS/slideshow.js"></script>
