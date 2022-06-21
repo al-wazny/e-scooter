400618430cc96af3f11f5405fbf5db4257fc5d26
ali.al-wazny <ali.al-wazny@flagbit.de>
2022-06-21 10:13:06 +0200

    chore: commit chages before merging with another branch

--- views/pages/scooter.php
+++ views/pages/scooter.php
@@ -1,37 +1,32 @@
-
-<?php 
+<?php
 include('../../app/controllers/Pages.php'); ?>
 <html>
-
-
 <head>
-    <link rel="stylesheet" href="../../assets/css/styles.css">
-    <link rel="stylesheet" href="../../assets/css/scooter1.css">
+     <link rel="stylesheet" href="../../assets/css/scooter1.css">
 </head>
 <body>
-    <header>
-        <a class="CompanyLogo" href="../index.html"> 
-            <img class="CompanyLogo" src="../../assets/Images/CompanyLogo.png" alt="Company Logo">
-        </a>
-        <a class="login" href=" " target="_top">Anmelden</a>
-       
-        <div class="bar">
-            <div class="Categoriesbar">
-                <h3 class="Categories">Categories</h3>
-            </div>   
-         </div>
-    </header>
+<header>
+    <?php
+    include('../pagelets/header.php');
+    ?>
+ </header>
     <div class="scooter">
         <div class="scootername">
             <br>
             <img src="<?= "data:image/jpeg;base64,".base64_encode($data->images) ?>" alt="">
         </div>
             <div class="information">
-                <h4><?= $data->title ?></h4>
-                <b><?= $data->price?><b><br>
-                <b><?= $data->description?></b>
+                <div class="title">
+                    <h4><?= $data->title ?></h4>
+                </div>
+                <div class="description">
+                    <span><?= $data->description?></span>
+                </div>
+                <div class="price">
+                    <b><span><?= $data->price?></span> Pro Tag<b><br>
+                </div>
                 <div class="scooterbutton 1">
-                    <button type=" button">Jetzt Mieten</button>
+                  <a href="miete.php"><button type=" button">Jetzt Mieten</button></a>
                 </div>
             </div>
     </div>
@@ -44,7 +39,7 @@ include('../../app/controllers/Pages.php'); ?>
             <b>RADGRÖSSE</b><br>
             <label for="RADGRÖSSE">10 Zoll</label><br><br>
             <b>MAXIMALE LEISTUNG</b><br>
-            <label for="MAXIMALE LEISTUNG">350 Watt</label><br><br>
+            <label for="MAXIMALE LEISTUNG"><?= $data->maxOutput ?> Watt</label><br><br>
             <b>GEWICHT</b><br>
             <label for="GEWICHT"><?= $data->title ?></label><br><br>
             <b>MAX. GESCHWINDIGKEIT</b><br>
