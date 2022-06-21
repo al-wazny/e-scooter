f6ba153c57cda69c99db2b17e7a4852c90681dde
SHA256:39EC0aSCPwY6nLGuP6aPwC0pwLIWwfzzxveWCbEO6J8 <ali.al-wazny@flagbit.de>
2022-06-21 23:10:36 +0200

    Revert "feat: display scooter info after renting and update data inside DB"
    This reverts commit 6bfd3fd025c38a9dfe0344171c157635eb62dd4c.

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_SESSION['username'] == '') {
-    header('location: views/pages/login.php');
-}
-// require_once 'app/boostrap.php';
-
-// new Core();
-header('location: views/index.php');
