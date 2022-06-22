fec61568c92e66df51de04a24a11f57f0f0e506c
SHA256:39EC0aSCPwY6nLGuP6aPwC0pwLIWwfzzxveWCbEO6J8 <ali.al-wazny@flagbit.de>
2022-06-22 23:45:50 +0200

    remove paragraph

--- views/pagelets/header.php
+++ views/pagelets/header.php
@@ -19,7 +19,7 @@
                    <img class="CompanyLogo" src="../../assets/Images/AtomLogo.png" alt="Company Logo">
                 </a>
 
-                <p class="username"><?php if(!isset($_SESSION['username']) || $_SESSION['username'] == ''): ?></p>
+                <?php if(!isset($_SESSION['username']) || $_SESSION['username'] == ''): ?>
                     <a class="register" href="../pages/registrate.php " target="_top" >Registrieren</a>
                     <a class="login" href="../pages/login.php " target="_top">Einloggen</a> 
                 <?php else: ?>
