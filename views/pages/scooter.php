2375d7d53d8bff470b7fb7346eb998d29f10813b
anwar.chaban <anwar.chaban@ktc.de>
2022-06-22 03:23:46 +0200

    update scooter CSS

--- views/pages/scooter.php
+++ views/pages/scooter.php
@@ -3,8 +3,6 @@ include('../../app/controllers/Pages.php'); ?>
 <html>
 <head>
      <link rel="stylesheet" href="../../assets/css/scooter1.css?v=<?php echo time(); ?>">
-  <link rel="stylesheet" href="../../assets/css/styles.css?v=<?php echo time(); ?>">
-
 </head>
 <body>
 <header>
@@ -21,6 +19,9 @@ include('../../app/controllers/Pages.php'); ?>
             <div class="information">
                 <div class="title">
                     <h4><?= $data->title ?></h4>
+                </div>
+                <div class="scooterbutton 1">
+                    <button type="submit"><span>Jetzt Mieten</span></button>
                 </div>
                 <div class="description">
                     <span><?= $data->description?></span>
@@ -39,29 +40,28 @@ include('../../app/controllers/Pages.php'); ?>
                 </div>
                 <div class="price">
                     <b><span><?= $data->price?></span> Pro Tag<b><br>
-                    </div>
-                    <div class="scooterbutton 1">
-                        <button type="submit">Jetzt Mieten</button>
-                    </div>
                 </div>
+                </div>
+                <div class="scooterinformation">
+                        <div class="scooterInformnationText">
+                            <h2>Information</h2>
+                            <div class="scooterinformation1">
+                                <b>AKKUTYP</b><br>
+                                <label for="AKKUTYP">551 Wh</label><br><br>
+                                <b>RADGRÖSSE</b><br>
+                                <label for="RADGRÖSSE">10 Zoll</label><br><br>
+                                <b>MAXIMALE LEISTUNG</b><br>
+                                <label for="MAXIMALE LEISTUNG"><?= $data->maxOutput ?> Watt</label><br><br>
+                                <b>GEWICHT</b><br>
+                                <label for="GEWICHT"><?= $data->title ?></label><br><br>
+                                <b>MAX. GESCHWINDIGKEIT</b><br>
+                                <label for="MAX. GESCHWINDIGKEIT"><?= $data->maxSpeed ?></label><br><br>
+                            </div>
+                        </div>
+                    </div>
             </div>
-        </form>
-<div class="scooterinformation">
-    <div class="scooterInformnationText">
-        <h2>Information</h2>
-        <div class="scooterinformation1">
-            <b>AKKUTYP</b><br>
-            <label for="AKKUTYP">551 Wh</label><br><br>
-            <b>RADGRÖSSE</b><br>
-            <label for="RADGRÖSSE">10 Zoll</label><br><br>
-            <b>MAXIMALE LEISTUNG</b><br>
-            <label for="MAXIMALE LEISTUNG"><?= $data->maxOutput ?> Watt</label><br><br>
-            <b>GEWICHT</b><br>
-            <label for="GEWICHT"><?= $data->title ?></label><br><br>
-            <b>MAX. GESCHWINDIGKEIT</b><br>
-            <label for="MAX. GESCHWINDIGKEIT"><?= $data->maxSpeed ?></label><br><br>
         </div>
-    </div>
-</div>
+        </form>
+
 </body>
 </html>
